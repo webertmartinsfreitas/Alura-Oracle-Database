63008e104ba686e2989cf2c9a3d7ebaa3f71edfc
webertfreitas <webert.freitas.jf@gmail.com>
2022-12-13 20:08:22 -0300

    Script de inserção de registros na tabela Clientes.

---
+++ Database-v1/Insert-Table-Clientes-v1.sql
@@ -0,0 +1,36 @@
+﻿-- Inserção de registros na tabela Clientes.
+INSERT INTO system.tb_clientes (
+    nr_cpf,
+    nm_nome_cliente,
+    nr_cep,
+    nm_logradouro,
+    nr_numero,
+    nm_complemento,
+    nm_bairro,
+    nm_cidade,
+    nm_estado,
+    dt_nascimento,
+    nr_idade,
+    nm_sexo,
+    nr_limete_credito,
+    nr_volume_compra,
+    nr_primeira_compra
+) VALUES (
+    '11601872677',
+    'Webert Martins de Freitas',
+    '36051520',
+    'Rua Teodorico de Assis',
+    '14',
+    'Apartamento 201',
+    'Manoel Honório',
+    'Juiz de Fora',
+    'MG',
+    TO_DATE('16/10/91', 'DD/MM/RR'),
+    '31',
+    'M',
+    '200000',
+    '5000',
+    '0'
+);
+
+COMMIT;
