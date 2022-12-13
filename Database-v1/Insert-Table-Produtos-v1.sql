50bd34abe850b752426e7b1aee65ebe7ae9571a2
webertfreitas <webert.freitas.jf@gmail.com>
2022-12-13 20:11:45 -0300

    Script de inserção de registro na tabela Produtos.

---
+++ Database-v1/Insert-Table-Produtos-v1.sql
@@ -0,0 +1,530 @@
+﻿-- Inserção de registros na tabela Produtos.
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1040107',
+    'Light',
+    'Lata',
+    '350 ml',
+    'Melância',
+    '4,56'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1037797',
+    'Clean',
+    'Lata',
+    '350 ml',
+    'Laranja',
+    '5,5'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1000889',
+    'Sabor da Montanha',
+    'Garrafa',
+    '700 ml',
+    'Uva',
+    '6,31'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1088126',
+    'Linha Citros',
+    'PET',
+    '1 Litro',
+    'Limão',
+    '7,004'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '544931',
+    'Frescor do Verão',
+    'Lata',
+    '350 ml',
+    'Limão',
+    '2,4595'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1078680',
+    'Frescor do Verão',
+    'Garrafa',
+    '470 ml',
+    'Manga',
+    '5,1795'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1042712',
+    'Linha Citros',
+    'Garrafa',
+    '700 ml',
+    'Limão',
+    '4,904'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '788975',
+    'Pedaços de Frutas',
+    'PET',
+    '1,5 Litros',
+    'Maça',
+    '1,8011'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1002767',
+    'Videira do Campo',
+    'Garrafa',
+    '700 ml',
+    'Cereja/Maça',
+    '8,41'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '231776',
+    'Festival de Sabores',
+    'Garrafa',
+    '700 ml',
+    'Açai',
+    '13,312'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '479745',
+    'Clean',
+    'Garrafa',
+    '470 ml',
+    'Laranja',
+    '3,768'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1051518',
+    'Frescor do Verão',
+    'Garrafa',
+    '470 ml',
+    'Limão',
+    '3,2995'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1101035',
+    'Linha Refrescante',
+    'PET',
+    '1 Litro',
+    'Morango/Limão',
+    '9,0105'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '229900',
+    'Pedaços de Frutas',
+    'Lata',
+    '350 ml',
+    'Maça',
+    '4,211'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1086543',
+    'Linha Refrescante',
+    'PET',
+    '1 Litro',
+    'Manga',
+    '11,0105'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '695594',
+    'Festival de Sabores',
+    'PET',
+    '1,5 Litros',
+    'Açai',
+    '2,8512'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '838819',
+    'Clean',
+    'PET',
+    '1,5 Litros',
+    'Laranja',
+    '1,2008'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '326779',
+    'Linha Refrescante',
+    'PET',
+    '1,5 Litros',
+    'Manga',
+    '16,5105'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '520380',
+    'Pedaços de Frutas',
+    'PET',
+    '1 Litro',
+    'Maça',
+    '12,011'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1041119',
+    'Linha Citros',
+    'Garrafa',
+    '700 ml',
+    'Lima/Limão',
+    '4,904'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '243083',
+    'Festival de Sabores',
+    'PET',
+    '1,5 Litros',
+    'Maracujá',
+    '10,512'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '394479',
+    'Sabor da Montanha',
+    'Garrafa',
+    '700 ml',
+    'Cereja',
+    '8,409'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '746596',
+    'Light',
+    'PET',
+    '1,5 Litros',
+    'Melância',
+    '19,505'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '773912',
+    'Clean',
+    'PET',
+    '1 Litro',
+    'Laranja',
+    '8,005'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '723457',
+    'Festival de Sabores',
+    'Garrafa',
+    '700 ml',
+    'Maracujá',
+    '4,912'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '812829',
+    'Clean',
+    'Lata',
+    '350 ml',
+    'Laranja',
+    '2,808'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '290478',
+    'Videira do Campo',
+    'Lata',
+    '350 ml',
+    'Melância',
+    '4,56'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '783663',
+    'Sabor da Montanha',
+    'Garrafa',
+    '700 ml',
+    'Morango',
+    '7,709'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '235653',
+    'Frescor do Verão',
+    'Lata',
+    '350 ml',
+    'Manga',
+    '3,8595'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1002334',
+    'Linha Citros',
+    'PET',
+    '1 Litro',
+    'Lima/Limão',
+    '7'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1013793',
+    'Videira do Campo',
+    'PET',
+    '2 Litros',
+    'Cereja/Maça',
+    '2,401'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1096818',
+    'Linha Refrescante',
+    'Garrafa',
+    '700 ml',
+    'Manga',
+    '7,7105'
+);
+
+INSERT INTO system.tb_produtos (
+    cd_produto,
+    nm_produto,
+    nm_embalagem,
+    nm_tamanho,
+    nm_sabor,
+    nr_preco_lista
+) VALUES (
+    '1022450',
+    'Festival de Sabores',
+    'PET',
+    '2 Litros',
+    'Açai',
+    '3,8012'
+);
+
+COMMIT;
