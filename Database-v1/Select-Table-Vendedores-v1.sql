624b885738d0811ec87020ca20f3bc8726ad2995
webertfreitas <webert.freitas.jf@gmail.com>
2022-12-13 20:56:55 -0300

    Script de seleção de registros na tabela Vendedores.

---
+++ Database-v1/Select-Table-Vendedores-v1.sql
@@ -0,0 +1,10 @@
+﻿-- Seleção dos registros na tabela Vendedores.
+SELECT
+    nr_matricula_vendedor  AS "Nº Matrícula",
+    nm_vendedor            AS "Nome do Vendedor",
+    dt_admissao_vendedor   AS "Data da Admissão",
+    nr_percentual_comissao AS "Comissão (%)"
+FROM
+    tb_vendedores
+ORDER BY
+    nm_vendedor ASC
