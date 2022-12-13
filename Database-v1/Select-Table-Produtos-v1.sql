a098c9d91fc316f2a74e7e69a6b1167c7b874be5
webertfreitas <webert.freitas.jf@gmail.com>
2022-12-13 20:53:46 -0300

    Script de seleção de registros na tabela Produtos.

---
+++ Database-v1/Select-Table-Produtos-v1.sql
@@ -0,0 +1,12 @@
+﻿-- Seleção dos registros na tabela Produtos.
+SELECT
+    cd_produto     AS "SKU",
+    nm_produto     AS "Nome do Produto",
+    nm_embalagem   AS "Embalagem",
+    nm_tamanho     AS "Tamanho",
+    nm_sabor       AS "Sabor",
+    nr_preco_lista AS "Preço de Tabela"
+FROM
+    tb_produtos
+ORDER BY
+    nm_produto ASC
